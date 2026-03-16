a23194161029d8e87271b9cd65677e8ba3b73bf2
nmartens40-rrc <Nmartens40@rrc.ca>
2026-03-16 14:46:33 -0500

    Fixed server module errors.

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -4,11 +4,6 @@ import path from "path";
 import cors from "cors";
 import { Worker } from "worker_threads";
 
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const port = 5000;
 
